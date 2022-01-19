448dd5f349fd5cbdc625a110e53e5cf57a271f36
plknk <pavel.konkin.dev@gmail.com>
2022-01-20 00:29:33 +0300

    Max Consecutive Ones Refactoring

--- src/main/java/com/pl2kn/leetcode/explore/arrays/MaxConsecutiveOnes.java
+++ src/main/java/com/pl2kn/leetcode/explore/arrays/MaxConsecutiveOnes.java
@@ -3,20 +3,16 @@ package com.pl2kn.leetcode.explore.arrays;
 public class MaxConsecutiveOnes {
 
   public int findMaxConsecutiveOnes(int[] nums) {
-    int length = nums.length;
     int maxOnes = 0;
     int currOnes = 0;
-    for (int i = 0; i < length; i++) {
-      if (nums[i] == 1) {
+    for (int num : nums) {
+      if (num == 1) {
         currOnes++;
-      }
-      if (nums[i] != 1 || i == length - 1) {
-        if (currOnes > maxOnes) {
-          maxOnes = currOnes;
-        }
+      } else {
+        maxOnes = Math.max(maxOnes, currOnes);
         currOnes = 0;
       }
     }
-    return maxOnes;
+    return Math.max(maxOnes, currOnes);
   }
 }
