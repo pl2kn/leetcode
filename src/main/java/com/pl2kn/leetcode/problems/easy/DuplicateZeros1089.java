b4b5d6a5a722468b89a425e84aa1a77b4e79ab77
plknk <pavel.konkin.dev@gmail.com>
2022-01-21 13:05:31 +0300

    1089. Duplicate Zeros. Better O(n) solution.

--- src/main/java/com/pl2kn/leetcode/problems/easy/DuplicateZeros1089.java
+++ src/main/java/com/pl2kn/leetcode/problems/easy/DuplicateZeros1089.java
@@ -3,14 +3,31 @@ package com.pl2kn.leetcode.problems.easy;
 public class DuplicateZeros1089 {
 
   public void duplicateZeros(int[] arr) {
-    int length = arr.length;
-    for (int i = 0; i < length - 1; i++) {
-      if (arr[i] == 0) {
-        for (int j = length - 1; j > i; j--) {
-          arr[j] = arr[j - 1];
-        }
-        arr[++i] = 0;
+    int zeroCount = 0;
+    for (int j : arr) {
+      if (j == 0) {
+        zeroCount++;
       }
+    }
+    int length = arr.length;
+    int i = length - 1;
+    int j = length + zeroCount - 1;
+    while (i < j) {
+      if (arr[i] != 0) {
+        if (j < length) {
+          arr[j] = arr[i];
+        }
+      } else {
+        if (j < length) {
+          arr[j] = arr[i];
+        }
+        j--;
+        if (j < length) {
+          arr[j] = arr[i];
+        }
+      }
+      i--;
+      j--;
     }
   }
 }
