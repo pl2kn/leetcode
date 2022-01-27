79bc72a4f2efb1d0c182521b1b485943e3618faf
plknk <pavel.konkin.dev@gmail.com>
2022-01-27 11:34:44 +0300

    328. Odd Even Linked List. Better solution.

--- src/main/java/com/pl2kn/leetcode/problems/medium/OddEvenLinkedList328.java
+++ src/main/java/com/pl2kn/leetcode/problems/medium/OddEvenLinkedList328.java
@@ -5,28 +5,19 @@ import com.pl2kn.leetcode.problems.common.ListNode;
 public class OddEvenLinkedList328 {
 
   public ListNode oddEvenList(ListNode head) {
-    if (head == null || head.next == null || head.next.next == null) {
-      return head;
+    if (head == null) {
+      return null;
     }
-    ListNode oddSentinel = new ListNode(0);
-    ListNode currentOdd = oddSentinel;
-    ListNode evenSentinel = new ListNode(0);
-    ListNode currentEven = evenSentinel;
-    ListNode current = head;
-    int index = 1;
-    while (current != null) {
-      ListNode next = current.next;
-      current.next = null;
-      if (index++ % 2 == 0) {
-        currentEven.next = current;
-        currentEven = current;
-      } else {
-        currentOdd.next = current;
-        currentOdd = current;
-      }
-      current = next;
+    ListNode odd = head;
+    ListNode even = head.next;
+    ListNode oddHead = even;
+    while (even != null && even.next != null) {
+      odd.next = even.next;
+      odd = odd.next;
+      even.next = odd.next;
+      even = even.next;
     }
-    currentOdd.next = evenSentinel.next;
-    return oddSentinel.next;
+    odd.next = oddHead;
+    return head;
   }
 }
--- src/test/java/com/pl2kn/leetcode/problems/medium/OddEvenLinkedList328Test.java
+++ src/test/java/com/pl2kn/leetcode/problems/medium/OddEvenLinkedList328Test.java
@@ -27,6 +27,23 @@ public class OddEvenLinkedList328Test {
   }
 
   @Test
+  public void Even_Test() {
+    ListNode a1 = new ListNode(1);
+    ListNode a2 = new ListNode(2);
+    ListNode a3 = new ListNode(3);
+    ListNode a4 = new ListNode(4);
+    ListNode a5 = new ListNode(5);
+    ListNode a6 = new ListNode(6);
+    a1.next = a2;
+    a2.next = a3;
+    a3.next = a4;
+    a4.next = a5;
+    a5.next = a6;
+    OddEvenLinkedList328 solution = new OddEvenLinkedList328();
+    assertEquals(a1, solution.oddEvenList(a1));
+  }
+
+  @Test
   public void Null_Test() {
     OddEvenLinkedList328 solution = new OddEvenLinkedList328();
     assertNull(solution.oddEvenList(null));
