bf3adf63fcc153723bb466143c8de9879e1ff80d
plknk <pavel.konkin.dev@gmail.com>
2022-01-29 20:20:50 +0300

    21. Merge Two Sorted Lists. Better solution.

--- src/main/java/com/pl2kn/leetcode/problems/easy/MergeTwoSortedLists21.java
+++ src/main/java/com/pl2kn/leetcode/problems/easy/MergeTwoSortedLists21.java
@@ -7,29 +7,17 @@ public class MergeTwoSortedLists21 {
   public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
     ListNode sentinel = new ListNode(0);
     ListNode currentHead = sentinel;
-    ListNode current1 = list1;
-    ListNode current2 = list2;
-    while (current1 != null && current2 != null) {
-      if (current1.val < current2.val) {
-        currentHead.next = current1;
-        currentHead = current1;
-        current1 = current1.next;
+    while (list1 != null && list2 != null) {
+      if (list1.val < list2.val) {
+        currentHead.next = list1;
+        list1 = list1.next;
       } else {
-        currentHead.next = current2;
-        currentHead = current2;
-        current2 = current2.next;
+        currentHead.next = list2;
+        list2 = list2.next;
       }
+      currentHead = currentHead.next;
     }
-    while (current1 != null) {
-      currentHead.next = current1;
-      currentHead = current1;
-      current1 = current1.next;
-    }
-    while (current2 != null) {
-      currentHead.next = current2;
-      currentHead = current2;
-      current2 = current2.next;
-    }
+    currentHead.next = list1 == null ? list2 : list1;
     return sentinel.next;
   }
 }
