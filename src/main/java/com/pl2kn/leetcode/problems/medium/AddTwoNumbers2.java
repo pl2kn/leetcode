018058f7a6631945e5def892b04917f281e0d9a3
plknk <pavel.konkin.dev@gmail.com>
2022-01-29 22:41:09 +0300

    2. Add Two Numbers. My solution refactoring.

--- src/main/java/com/pl2kn/leetcode/problems/medium/AddTwoNumbers2.java
+++ src/main/java/com/pl2kn/leetcode/problems/medium/AddTwoNumbers2.java
@@ -7,25 +7,23 @@ public class AddTwoNumbers2 {
   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
     ListNode sentinel = new ListNode(-1);
     ListNode currentDigit = sentinel;
-    int decade = 0;
-    while (l1 != null && l2 != null) {
-      int value = l1.val + l2.val + decade;
-      currentDigit.next = new ListNode(value % 10);
+    int carry = 0;
+    while (l1 != null || l2 != null) {
+      int x = (l1 != null) ? l1.val : 0;
+      int y = (l2 != null) ? l2.val : 0;
+      int sum = x + y + carry;
+      currentDigit.next = new ListNode(sum % 10);
       currentDigit = currentDigit.next;
-      decade = value / 10;
-      l1 = l1.next;
-      l2 = l2.next;
+      carry = sum / 10;
+      if (l1 != null) {
+        l1 = l1.next;
+      }
+      if (l2 != null) {
+        l2 = l2.next;
+      }
     }
-    ListNode extraHead = l1 != null ? l1 : l2;
-    while (extraHead != null) {
-      int value = extraHead.val + decade;
-      currentDigit.next = new ListNode(value % 10);
-      currentDigit = currentDigit.next;
-      decade = value / 10;
-      extraHead = extraHead.next;
-    }
-    if (decade > 0) {
-      currentDigit.next = new ListNode(decade);
+    if (carry > 0) {
+      currentDigit.next = new ListNode(carry);
     }
     return sentinel.next;
   }
