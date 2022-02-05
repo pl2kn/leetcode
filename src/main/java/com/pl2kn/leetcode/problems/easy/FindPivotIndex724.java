0d62ad7ddb71ac043047db018175083f5001351e
plknk <pavel.konkin.dev@gmail.com>
2022-02-05 18:23:58 +0300

    724. Find Pivot Index. Better soluion.

--- src/main/java/com/pl2kn/leetcode/problems/easy/FindPivotIndex724.java
+++ src/main/java/com/pl2kn/leetcode/problems/easy/FindPivotIndex724.java
@@ -3,15 +3,16 @@ package com.pl2kn.leetcode.problems.easy;
 public class FindPivotIndex724 {
 
   public int pivotIndex(int[] nums) {
-    int length = nums.length;
-    int[] sums = new int[length + 1];
-    for (int i = 1; i < sums.length; i++) {
-      sums[i] = sums[i - 1] + nums[i - 1];
+    int sum = 0;
+    for (int num : nums) {
+      sum += num;
     }
-    for (int i = 0; i < sums.length - 1; i++) {
-      if (sums[i] == sums[sums.length - 1] - sums[i] - nums[i]) {
+    int leftSum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (leftSum == sum - leftSum - nums[i]) {
         return i;
       }
+      leftSum += nums[i];
     }
     return -1;
   }
