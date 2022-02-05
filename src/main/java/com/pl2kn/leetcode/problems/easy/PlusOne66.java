158b882875c48a6754af4fc0d7ecc5eb3320e054
plknk <pavel.konkin.dev@gmail.com>
2022-02-05 19:25:31 +0300

    66. Plus One. Better solution.

--- src/main/java/com/pl2kn/leetcode/problems/easy/PlusOne66.java
+++ src/main/java/com/pl2kn/leetcode/problems/easy/PlusOne66.java
@@ -3,20 +3,15 @@ package com.pl2kn.leetcode.problems.easy;
 public class PlusOne66 {
 
   public int[] plusOne(int[] digits) {
-    int carry = 1;
     for (int i = digits.length - 1; i >= 0; i--) {
-      int sum = digits[i] + carry;
-      digits[i] = sum % 10;
-      carry = sum / 10;
-    }
-    if (carry > 0) {
-      int[] result = new int[digits.length + 1];
-      result[0] = carry;
-      for (int i = 0; i < digits.length; i++) {
-        result[i + 1] = digits[i];
+      if (digits[i] < 9) {
+        digits[i] += 1;
+        return digits;
       }
-      return result;
+      digits[i] = 0;
     }
+    digits = new int[digits.length + 1];
+    digits[0] = 1;
     return digits;
   }
 }
