8a00e9b1a4c581152546682a83698229f045d21c
pl2kn <pavel.konkin.dev@gmail.com>
2022-05-16 23:04:41 +0300

    1137. N-th Tribonacci Number. Better solution.

--- src/main/java/com/pl2kn/leetcode/problems/easy/NthTribonacciNumber1137BottomUp.java
+++ src/main/java/com/pl2kn/leetcode/problems/easy/NthTribonacciNumber1137BottomUp.java
@@ -3,14 +3,10 @@ package com.pl2kn.leetcode.problems.easy;
 public class NthTribonacciNumber1137BottomUp {
 
   public int tribonacci(int n) {
-    int[] dp = new int[n + 1];
-
-    dp[0] = 0;
-    dp[1] = 1;
-    dp[2] = 1;
+    int[] dp = {0, 1, 1};
 
     for (int i = 3; i <= n; i++) {
-      dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+      dp[i % 3] = dp[0] + dp[1] + dp[2];
     }
 
     return dp[n];
