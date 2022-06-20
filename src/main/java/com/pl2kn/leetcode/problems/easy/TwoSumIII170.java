d37a673ad89b11cb4efccae9023557929591e196
pl2kn <pavel.konkin.dev@gmail.com>
2022-06-20 18:16:11 +0300

    170. Two Sum III - Data structure design

---
+++ src/main/java/com/pl2kn/leetcode/problems/easy/TwoSumIII170.java
@@ -0,0 +1,36 @@
+package com.pl2kn.leetcode.problems.easy;
+
+import com.sun.org.apache.xpath.internal.objects.XNumber;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumIII170 {
+
+  class TwoSum {
+
+    private final Map<Integer, Integer> map;
+
+    public TwoSum() {
+      map = new HashMap<>();
+    }
+
+    public void add(int number) {
+      map.put(number, map.getOrDefault(number, 0) + 1);
+    }
+
+    public boolean find(int value) {
+      for (int number : map.keySet()) {
+        int key = value - number;
+        if (map.containsKey(key)) {
+          if (key != number) {
+            return true;
+          } else if (map.get(key) > 1) {
+            return true;
+          }
+        }
+      }
+
+      return false;
+    }
+  }
+}
