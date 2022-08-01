a1f1af17e57f61760def1636b3a5bfe4e27f3975
pl2kn <pavel.konkin.dev@gmail.com>
2022-08-01 20:21:56 +0300

    62. Unique Paths

--- src/main/java/com/pl2kn/leetcode/problems/medium/UniquePaths62Recursion.java
+++ src/main/java/com/pl2kn/leetcode/problems/medium/UniquePaths62Recursion.java
@@ -3,14 +3,6 @@ package com.pl2kn.leetcode.problems.medium;
 public class UniquePaths62Recursion {
 
   public int uniquePaths(int m, int n) {
-    return uniquePathsHelper(m - 1, n - 1);
-  }
-
-  private int uniquePathsHelper(int m, int n) {
-    if (m < 0 || n < 0) {
-      return 0;
-    }
-
     if (m == 0 || n == 0) {
       return 1;
     }
