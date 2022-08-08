921aa1c3b484767cbd85bc77416e80e64a62f870
pl2kn <pavel.konkin.dev@gmail.com>
2022-08-08 09:52:42 +0300

    252. Meeting Rooms

---
+++ src/main/java/com/pl2kn/leetcode/problems/medium/MeetingRooms252.java
@@ -0,0 +1,18 @@
+package com.pl2kn.leetcode.problems.medium;
+
+import java.util.Arrays;
+
+public class MeetingRooms252 {
+
+  public boolean canAttendMeetings(int[][] intervals) {
+    Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+    for (int i = 1; i < intervals.length; i++) {
+      if (intervals[i][0] < intervals[i - 1][1]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
