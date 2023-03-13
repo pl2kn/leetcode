36e821ebe0986499f0a1669a379a6e3db44e4a41
pl2kn <pavel.konkin.dev@gmail.com>
2023-03-13 11:07:29 +0300

    338. Counting Bits

---
+++ src/main/java/com/pl2kn/leetcode/problems/easy/CountingBits338.java
@@ -0,0 +1,14 @@
+package com.pl2kn.leetcode.problems.easy;
+
+public class CountingBits338 {
+
+  public int[] countBits(int n) {
+    int[] ans = new int[n + 1];
+
+    for (int i = 0; i <= n; i++) {
+      ans[i] = ans[i >> 1] + (i & 1);
+    }
+
+    return ans;
+  }
+}
