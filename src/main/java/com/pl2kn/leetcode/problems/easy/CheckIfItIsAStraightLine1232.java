a05a92fae8357fa44b7b21a102034ffc4977acf6
pl2kn <pavel.konkin.dev@gmail.com>
2023-06-06 00:49:51 +0300

    1232. Check If It Is a Straight Line

---
+++ src/main/java/com/pl2kn/leetcode/problems/easy/CheckIfItIsAStraightLine1232.java
@@ -0,0 +1,26 @@
+package com.pl2kn.leetcode.problems.easy;
+
+public class CheckIfItIsAStraightLine1232 {
+
+  public boolean checkStraightLine(int[][] coordinates) {
+    int deltaY = getYDiff(coordinates[1], coordinates[0]);
+    int deltaX = getXDiff(coordinates[1], coordinates[0]);
+
+    for (int i = 2; i < coordinates.length; i++) {
+      if (deltaY * getXDiff(coordinates[i], coordinates[0]) != deltaX * getYDiff(coordinates[i],
+          coordinates[0])) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  private int getYDiff(int[] a, int[] b) {
+    return a[1] - b[1];
+  }
+
+  private int getXDiff(int[] a, int[] b) {
+    return a[0] - b[0];
+  }
+}
