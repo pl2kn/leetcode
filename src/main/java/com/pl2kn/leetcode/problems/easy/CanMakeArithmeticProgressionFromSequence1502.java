c4c114c8a6a3829af83221db7d601234a18b6794
pl2kn <pavel.konkin.dev@gmail.com>
2023-06-06 23:02:28 +0300

    1502. Can Make Arithmetic Progression From Sequence. Better Solution.

--- src/main/java/com/pl2kn/leetcode/problems/easy/CanMakeArithmeticProgressionFromSequence1502.java
+++ src/main/java/com/pl2kn/leetcode/problems/easy/CanMakeArithmeticProgressionFromSequence1502.java
@@ -1,20 +1,39 @@
 package com.pl2kn.leetcode.problems.easy;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CanMakeArithmeticProgressionFromSequence1502 {
 
   public boolean canMakeArithmeticProgression(int[] arr) {
-    Arrays.sort(arr);
-
-    int diff = arr[1] - arr[0];
-
-    for (int i = 2; i < arr.length; i++) {
-      if (arr[i] - arr[i - 1] != diff) {
-        return false;
-      }
+    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
+    for (int num : arr) {
+      min = Math.min(min, num);
+      max = Math.max(max, num);
     }
 
-    return true;
+    if (max - min == 0) {
+      return false;
+    }
+
+    int n = arr.length;
+
+    if ((max - min) % (n - 1) != 0) {
+      return false;
+    }
+
+    int diff = (max - min) / (n - 1);
+    Set<Integer> numSet = new HashSet<>();
+
+    for (int num : arr) {
+      if ((num - min) % diff != 0) {
+        return false;
+      }
+      numSet.add(num);
+    }
+
+    return numSet.size() == n;
   }
 }
